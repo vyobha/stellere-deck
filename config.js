2d659a2ad9ed6664341ff883c1a6cf7d38546021
vyobha <vyobhacorp@gmail.com>
2026-07-30 19:21:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ─── EDIT THESE AFTER YOU DEPLOY THE APPS SCRIPT BACKEND (see SETUP.md) ───
 const CONFIG = {
   // Paste your Google Apps Script Web App URL here (SETUP.md, Part 2, step 7)
-  ANALYTICS_ENDPOINT: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  ANALYTICS_ENDPOINT: "https://script.google.com/macros/s/AKfycbytRmdLG7NoHaaaMQFE43DP41oiLUVtipl8QDDtTpC7pPjrlXdIkK0SwGcivIAMXmA1/exec",
 
   // The Google Drive prototype video link, pulled from your deck's "Click here for the video" link
   VIDEO_URL: "https://drive.google.com/file/d/1tYnk-QRqThu2AIjNPTU7kQtEshhAvnw5/view?usp=sharing",
